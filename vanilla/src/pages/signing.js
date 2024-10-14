1c3251d3d6fde0ce96f20ec22dcea8866ed7e6ff
tomikorzu <tomykorzu@icloud.com>
2024-10-13 22:51:06 -0300

    feat(signin.js): add signin page

--- vanilla/src/pages/signing.js
+++ vanilla/src/pages/signing.js
@@ -4,42 +4,7 @@ import { redirectPage } from "../utils/mainFunctions";
 const Signin = () => {
   changePageSetting("Home - Sign-In", "../../public/vite.svg");
 
-  document.querySelector("#app").innerHTML = `
-  <div class="back-drop">
-        <main class="fade-in main-center">
-          <form class="form-class form" id="sign-in-form">
-          <h2 class="form-title">Sign In</h2>
-          <div class="inputs-container">
-          <input
-          type="text"
-          class="input-form input"
-          id="email-username"
-          placeholder="Email or User Name"
-          />
-          <input
-          type="password"
-          class="input-form input"
-          id="password"
-          placeholder="Password"
-          />
-          </div>
-          <button
-              type="submit"
-              id="submit-signin-btn"
-              class="accept-btn form-btn btn"
-              >
-              Submit
-              </button>
-              <p class="already-account-p">
-              Aleready you don't have an account?
-              <button class="already-account-btn" id="go-to-sign-up">
-              Sign up
-              </button>
-              </p>
-              </form>
-              </main>
-              </div>
-              `;
+  signinLayout();
 
   const goToSignUpBtn = document.getElementById("go-to-sign-up");
   goToSignUpBtn.addEventListener("click", (e) => {
@@ -58,6 +23,45 @@ const Signin = () => {
       console.log("All inputs are correct");
     }
   });
+};
+
+const signinLayout = () => {
+  document.querySelector("#app").innerHTML = `
+    <div class="back-drop">
+          <main class="fade-in main-center">
+            <form class="form-class form" id="sign-in-form">
+            <h2 class="form-title">Sign In</h2>
+            <div class="inputs-container">
+            <input
+            type="text"
+            class="input-form input"
+            id="email-username"
+            placeholder="Email or User Name"
+            />
+            <input
+            type="password"
+            class="input-form input"
+            id="password"
+            placeholder="Password"
+            />
+            </div>
+            <button
+                type="submit"
+                id="submit-signin-btn"
+                class="accept-btn form-btn btn"
+                >
+                Submit
+                </button>
+                <p class="already-account-p">
+                Aleready you don't have an account?
+                <button class="already-account-btn" id="go-to-sign-up">
+                Sign up
+                </button>
+                </p>
+                </form>
+                </main>
+                </div>
+                `;
 };
 
 export default Signin;
